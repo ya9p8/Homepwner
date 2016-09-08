95aca8a5bc9c40895fced60c7e3228c7c25bcb1d
Yemi Ajibola <ya9p8@mst.edu>
2016-09-08 13:48:08 -0500

    Added item tableview controller. Can add and delete items.

---
+++ Homepwner/ItemStore.swift
@@ -0,0 +1,43 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Yemi Ajibola on 9/7/16.
+//  Copyright © 2016 Yemi Ajibola. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    
+    func removeItem(item: Item) {
+        if let index = allItems.indexOf(item) {
+            allItems.removeAtIndex(index)
+        }
+    }
+    
+    func moveItemAtIndex(fromIndex: Int, toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        
+        // Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        
+        // Remove item from array
+        allItems.removeAtIndex(fromIndex)
+        
+        // Insert item in array at new location
+        allItems.insert(movedItem, atIndex: toIndex)
+        
+    }
+}
