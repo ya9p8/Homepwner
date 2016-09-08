45999b18b6e0cefc0a7f68ca2a89dae2c018f98b
Yemi Ajibola <ya9p8@mst.edu>
2016-09-07 19:38:17 -0500

    Added item class and Item tableview controller.

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Yemi Ajibola on 9/7/16.
+//  Copyright © 2016 Yemi Ajibola. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
