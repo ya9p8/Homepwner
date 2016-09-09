72b4d27903243269c00e37de9a00e0793d8c20f5
Yemi Ajibola <ya9p8@mst.edu>
2016-09-08 22:11:19 -0500

    Item passed to detail view controller.

--- Homepwner/DetailViewController.swift
+++ Homepwner/DetailViewController.swift
@@ -7,3 +7,36 @@
 //
 
 import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var serialNumberTextField: UITextField!
+    @IBOutlet weak var valueTextField: UITextField!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var item: Item!
+    
+    let numberFormatter: NSNumberFormatter = {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .DecimalStyle
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        return formatter
+    }()
+    
+    let dateFormattter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .MediumStyle
+        formatter.timeStyle = .NoStyle
+        return formatter
+    }()
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nameTextField.text = item.name
+        serialNumberTextField.text = item.serialNumber
+        valueTextField.text = numberFormatter.stringFromNumber(item.valueInDollars)
+        dateLabel.text = dateFormattter.stringFromDate(item.dateCreated)
+    }
+}
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -117,4 +117,19 @@ class ItemsViewController: UITableViewController {
         }
         
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // If the triggered segue is the "ShowItem" segue
+        if segue.identifier == "ShowItem" {
+            // Figure out which row was just tapped
+            if let row = tableView.indexPathForSelectedRow?.row {
+                
+                // Get the item associated with this row and pass it along
+                let item = itemStore.allItems[row]
+                let detailViewController = segue.destinationViewController as! DetailViewController
+                detailViewController.item = item
+                
+            }
+        }
+    }
 }
